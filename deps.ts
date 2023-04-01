6c63328124156ee434ae0792ff7453aa91046d47
Francisco Ramos <fro.profesional@gmail.com>
2023-03-31 22:13:30 -0700

    Update api-core

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 export {
   API,
   ApiRouter,
-} from "https://raw.githubusercontent.com/clau-org/api-core/v0.0.5/src/api.ts";
-export { validate } from "https://raw.githubusercontent.com/clau-org/api-core/v0.0.5/src/middleware/validate.ts";
+} from "https://raw.githubusercontent.com/clau-org/api-core/v0.0.6/src/api.ts";
+export { validate } from "https://raw.githubusercontent.com/clau-org/api-core/v0.0.6/src/middleware/validate.ts";
 export { Context } from "https://deno.land/x/oak@v12.1.0/mod.ts";
 export { Schema, z, ZodError } from "https://deno.land/x/zod@v3.21.4/mod.ts";
 export * as JWT from "https://deno.land/x/djwt@v2.8/mod.ts";
